cd32a13563050a8e006c95542170a82e3ee90c24
Ali_HZ <alirezahajizade362@gmail.com>
2026-09-23 11:10:30 +0330

    feat(auth): add login controller and routes for user authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -35,4 +35,36 @@ const registerUser = async (req, res) => {
       .json({ message: "خطای سرور در ثبت‌نام", error: error.message });
   }
 };
-module.exports = { registerUser };
+
+const loginUser = async (req, res) => {
+  try {
+    const { phone, password } = req.body;
+    if (!phone || !password) {
+      return res
+        .statuse(400)
+        .json({ message: " لطفا شماره تفن و رمز عبور را وارد کنید " });
+    }
+    const user = await User.findOne({ phone });
+    if (!user || user.password !== password) {
+      return res
+        .statuse(401)
+        .json({
+          message: " کاربری با این  مشخصات یافت نشد یا رمز عبور اشتباه می باشد",
+        });
+    }
+    res.status(200).json({
+      message: " ورود با موفقیت انجام شد",
+      user: {
+        id: user._id,
+        fullName: user.fullName,
+        phone: user.phone,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "خطای سرور در ورود به حساب", error: error.message });
+  }
+};
+module.exports = { registerUser, loginUser };
--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,6 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("سلام! سرور کنکورینو با موفقست روشن شد");
-});
-app.post("/login", (req, res) => {
-  const { phone, password } = req.body;
-  res.send(`شماره${phone}با موفقیت دریافت شد!`);
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is runnig on port ${PORT}`);
